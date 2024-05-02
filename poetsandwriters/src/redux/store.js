593418a773769eac43ede96d9d09b1f65f759449
Aman <amanraghav682@gmail.com>
2024-05-02 19:16:35 +0530

    Redux persist

--- poetsandwriters/src/redux/store.js
+++ poetsandwriters/src/redux/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from './user/userSlice';
 import { useReducer } from "react";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { version } from "mongoose";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer= combineReducers({
+    user: userReducer,
+});
+
+const persistConfig= {
+    key: 'root',
+    storage,
+    version: 1,
+};
+const persistedReducer= persistReducer(persistConfig);
 
 export default store = configureStore({
-    reducer: {
-        user: useReducer,
-        
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({ serializableCheck: false }),
 })
+
+export const persistor= persistStore(store);
